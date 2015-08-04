774d860a7cec3547536cfe70b72db746f99c6469
jaeguly <jaeguly@gmail.com>
2015-08-04 11:07:40 +0900

    apply 'indent -kr -ci8 -cli0 -i4 -l100 -nut'.

--- readavi/fileio.c
+++ readavi/fileio.c
@@ -10,94 +10,89 @@ This code falls under the BSD license.
 #include <stdio.h>
 #include <stdlib.h>
 
-int write_long(FILE *out, int n)
+int write_long(FILE * out, int n)
 {
-  putc((n&255),out);
-  putc(((n>>8)&255),out);
-  putc(((n>>16)&255),out);
-  putc(((n>>24)&255),out);
+    putc((n & 255), out);
+    putc(((n >> 8) & 255), out);
+    putc(((n >> 16) & 255), out);
+    putc(((n >> 24) & 255), out);
 
-  return 0;
+    return 0;
 }
 
-int write_word(FILE *out, int n)
+int write_word(FILE * out, int n)
 {
-  putc((n&255),out);
-  putc(((n>>8)&255),out);
+    putc((n & 255), out);
+    putc(((n >> 8) & 255), out);
 
-  return 0;
+    return 0;
 }
 
-int read_long(FILE *in)
+int read_long(FILE * in)
 {
-int c;
+    int c;
 
-  c=getc(in);
-  c=c+(getc(in)<<8);
-  c=c+(getc(in)<<16);
-  c=c+(getc(in)<<24);
+    c = getc(in);
+    c = c + (getc(in) << 8);
+    c = c + (getc(in) << 16);
+    c = c + (getc(in) << 24);
 
-  return c;
+    return c;
 }
 
-int read_word(FILE *in)
+int read_word(FILE * in)
 {
-int c;
+    int c;
 
-  c=getc(in);
-  c=c+(getc(in)<<8);
+    c = getc(in);
+    c = c + (getc(in) << 8);
 
-  return c;
+    return c;
 }
 
-int read_chars(FILE *in, char *s, int count)
+int read_chars(FILE * in, char *s, int count)
 {
-int t;
+    int t;
 
-  for (t=0; t<count; t++)
-  {
-    s[t]=getc(in);
-  }
+    for (t = 0; t < count; t++) {
+        s[t] = getc(in);
+    }
 
-  s[t]=0;
+    s[t] = 0;
 
-  return 0;
+    return 0;
 }
 
-int read_chars_bin(FILE *in, char *s, int count)
+int read_chars_bin(FILE * in, char *s, int count)
 {
-int t;
+    int t;
 
-  for (t=0; t<count; t++)
-  {
-    s[t]=getc(in);
-  }
+    for (t = 0; t < count; t++) {
+        s[t] = getc(in);
+    }
 
-  return 0;
+    return 0;
 }
 
-int write_chars(FILE *out, char *s)
+int write_chars(FILE * out, char *s)
 {
-int t;
+    int t;
 
-  t=0;
-  while(s[t]!=0 && t<255)
-  {
-    putc(s[t++],out);
-  }
+    t = 0;
+    while (s[t] != 0 && t < 255) {
+        putc(s[t++], out);
+    }
 
-  return 0;
+    return 0;
 }
 
-int write_chars_bin(FILE *out, char *s, int count)
+int write_chars_bin(FILE * out, char *s, int count)
 {
-int t;
+    int t;
 
-  for (t=0; t<count; t++)
-  {
-    putc(s[t],out);
-  }
+    for (t = 0; t < count; t++) {
+        putc(s[t], out);
+    }
 
-  return 0;
+    return 0;
 }
-
--- readavi/fileio.h
+++ readavi/fileio.h
@@ -7,14 +7,13 @@ This code falls under the BSD license.
 
 */
 
-int write_long(FILE *out, int n);
-int write_word(FILE *out, int n);
+int write_long(FILE * out, int n);
+int write_word(FILE * out, int n);
 
-int read_long(FILE *in);
-int read_word(FILE *in);
+int read_long(FILE * in);
+int read_word(FILE * in);
 
-int read_chars(FILE *in, char *s, int count);
-int read_chars_bin(FILE *in, char *s, int count);
-int write_chars(FILE *out, char *s);
-int write_chars_bin(FILE *out, char *s, int count);
-
+int read_chars(FILE * in, char *s, int count);
+int read_chars_bin(FILE * in, char *s, int count);
+int write_chars(FILE * out, char *s);
+int write_chars_bin(FILE * out, char *s, int count);
